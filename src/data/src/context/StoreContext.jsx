52c30dff8e6d2fae8bf9fa01130abe3e374f3d81
Yash12227 <rathoreyash89@gmail.com>
2025-12-26 22:15:13 +0530

    Create StoreContext for state management

---
+++ src/data/src/context/StoreContext.jsx
@@ -0,0 +1,14 @@
+import { createContext, useReducer } from "react";
+import { reducer, initialState } from "./reducer";
+
+export const StoreContext = createContext();
+
+export const StoreProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <StoreContext.Provider value={{ state, dispatch }}>
+      {children}
+    </StoreContext.Provider>
+  );
+};
